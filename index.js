e16d2290e5fa4f2820cff8041b26324fac7fcdba
pedrohenriquepaixao <41974577+pedrohenriquepaixao@users.noreply.github.com>
2020-10-22 21:47:49 -0300

    Validação de campo vazio e gerar o db caso ele não existe

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
-import express from 'express';
-import { promises as fs} from 'fs';
-import shortid from 'shortid';
-import cors from 'cors';
+import express from "express";
+import { promises as fs } from "fs";
+import shortid from "shortid";
+import cors from "cors";
 
 const app = express();
 
 const { readFile, writeFile } = fs;
 
 app.use(cors());
-app.use(express.json())
-app.set('view engine', 'ejs')
-app.use(express.urlencoded( {extended: false }))
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
-    res.render('index', { shortUrls: data.Urls, urlServer: req.url});
-})
-
-app.get("/shortUrl", async (req, res) => {
-    res.send(data);
-})
-
-app.post("/", async (req, res) => {
-    try{
-        const { addr } = req.body;
-        const findIndex = data.Urls.filter(url => (url.addr === addr))
-        if(findIndex.length < 1){
-            let Url = {
-                id: data.NextId++,
-                shortUrl: `${shortid.generate()}`,
-                addr: addr,
-                numAccess: 0,
-            };
-        
-            data.Urls.push(Url);
-        
-            await writeFile('./db/bancoUrl.json', JSON.stringify(data,null,2))
-            res.redirect("./")
-            
-        }else{
-            res.send("O link enviado já esta cadastrado no banco de dados")
-        }        
-
-    }catch(err){
-        console.log(err.message)
-    }
-})
-
-app.get("/:shortUrl", async (req,res)=>{
-    
-    const findIndex = data.Urls.filter(url => (url.shortUrl === req.params.shortUrl));
-    if(findIndex < 1) return res.sendStatus(404);
-    findIndex[0].numAccess++;
-
-    await writeFile('./db/bancoUrl.json', JSON.stringify(data,null,2));
-    res.redirect(findIndex[0].addr);
-
+  res.render("index", { shortUrls: data.Urls, urlServer: req.url });
 });
 
+app.get("/shortUrl", async (req, res) => {
+  res.send(data);
+});
+
+app.post("/", async (req, res) => {
+  try {
+    if (req.body.addr) {
+      const { addr } = req.body;
+      const findIndex = data.Urls.filter((url) => url.addr === addr);
+      if (findIndex.length < 1) {
+        let Url = {
+          id: data.NextId++,
+          shortUrl: `${shortid.generate()}`,
+          addr: addr,
+          numAccess: 0,
+        };
+
+        data.Urls.push(Url);
+
+        await writeFile("./db/bancoUrl.json", JSON.stringify(data, null, 2));
+        res.render("index", { shortUrls: data.Urls, urlServer: req.url });
+      } else {
+        res.send("O link enviado já esta cadastrado no banco de dados");
+      }
+    } else {
+      res.render("error", {
+        errorMessage: "O campo de link deve ser preenchido!",
+      });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const findIndex = data.Urls.filter(
+    (url) => url.shortUrl === req.params.shortUrl
+  );
+  if (findIndex < 1) return res.sendStatus(404);
+  findIndex[0].numAccess++;
+
+  await writeFile("./db/bancoUrl.json", JSON.stringify(data, null, 2));
+  res.redirect(findIndex[0].addr);
+});
 
 app.listen(process.env.PORT || 3000, async () => {
-    console.log("Subiu o APP")
-    global.data = JSON.parse(await readFile('./db/bancoUrl.json'));
-    try{
-        await fs.open('./db/bancoUrl.json');
-    }catch(err){
-        console.log(err.message)
-    }
+  console.log("Subiu o APP");
 
-})
+  try {
+    await readFile("./db/bancoUrl.json");
+  } catch (err) {
+    try {
+      const base = {
+        NextId: 1,
+        Urls: [],
+      };
+
+      await writeFile("./db/bancoUrl.json", JSON.stringify(base, null, 2));
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  global.data = JSON.parse(await readFile("./db/bancoUrl.json"));
+});
